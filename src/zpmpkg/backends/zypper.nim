d3557d3583a341e14b94d1ae8e795d2df4f84b40
michal92299 <chudybyczek2137@gmail.com>
2026-08-26 12:59:46 +0200

    Update zypper.nim

--- src/zpmpkg/backends/zypper.nim
+++ src/zpmpkg/backends/zypper.nim
@@ -22,6 +22,11 @@ proc search*(query: string): seq[PackageCandidate] =
       installCmd: @["sudo", "zypper", "install", "-y", name], extra: "zypper"
     ))
 
+proc isInstalled*(name: string): bool =
+  if not isPresent(): return false
+  let (_, code) = runCapture("rpm", @["-q", name])
+  code == 0
+
 proc install*(name: string): int =
   runInteractive("sudo", @["zypper", "install", "-y", name])
 
